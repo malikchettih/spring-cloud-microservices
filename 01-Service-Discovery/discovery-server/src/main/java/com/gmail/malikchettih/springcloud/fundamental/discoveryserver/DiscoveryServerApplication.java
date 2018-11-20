b969a22e7e84a96dd36fd2131b3be74de5f1a23e
mchettih <malik.chettih@gmail.com>
2018-11-20 15:24:39 +0100

    Add discovery-server project

---
+++ 01-Service-Discovery/discovery-server/src/main/java/com/gmail/malikchettih/springcloud/fundamental/discoveryserver/DiscoveryServerApplication.java
@@ -0,0 +1,12 @@
+package com.gmail.malikchettih.springcloud.fundamental.discoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryServerApplication.class, args);
+	}
+}
